2a05fe7fb9ff3f1275442d300650c8aab226b07a
Abdul Bari <linked-blue@outlook.com>
2023-10-26 10:33:51 +0400

    Creates Runtime Master Processor Service. The purpose of this service is to run all processors with the use of reflection.

---
+++ HealthPartners.FizzBuzz/HealthPartners.FizzBuzz.ConsoleApplication/Services/RuntimeMasterProcessorService.cs
@@ -0,0 +1,12 @@
+﻿using HealthPartners.FizzBuzz.ConsoleApplication.Interfaces;
+
+namespace HealthPartners.FizzBuzz.ConsoleApplication.Services
+{
+    public class RuntimeMasterProcessorService : IMasterProcessorService
+    {
+        public IEnumerable<string> ProcessAll(int[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
