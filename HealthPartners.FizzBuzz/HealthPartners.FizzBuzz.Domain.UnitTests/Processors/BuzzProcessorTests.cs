c07e509e2d624d7aaf04019c664cd105b4f7811f
Abdul Bari <linked-blue@outlook.com>
2023-10-26 10:05:06 +0400

    Creates empty Buzz processor that inherits from IProcessor and creates failing test for BuzzProcessor to return buzz when a number is divisble by five

---
+++ HealthPartners.FizzBuzz/HealthPartners.FizzBuzz.Domain.UnitTests/Processors/BuzzProcessorTests.cs
@@ -0,0 +1,29 @@
+﻿using HealthPartners.FizzBuzz.Domain.Processors;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthPartners.FizzBuzz.Domain.UnitTests.Processors
+{
+    internal class BuzzProcessorTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void ProcessMethod_WhenProvidedANumberDivisbleByFive_ReturnsBuzz()
+        {
+            var buzzProcessor = new BuzzProcessor();
+            var numberDivisbleByFive = 5;
+
+            var result = buzzProcessor.Process(numberDivisbleByFive);
+            Assert.AreEqual("Buzz", result);
+        }
+    }
+}
---
+++ HealthPartners.FizzBuzz/HealthPartners.FizzBuzz.Domain/Processors/BuzzProcessor.cs
@@ -0,0 +1,19 @@
+﻿using HealthPartners.FizzBuzz.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthPartners.FizzBuzz.Domain.Processors
+{
+    public class BuzzProcessor : IProcessor
+    {
+        public byte OrderConfig => throw new NotImplementedException();
+
+        public string? Process(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
