37f242b72f6c4141e3f4daf4205f0e682ec5e0d9
Abdul Bari <linked-blue@outlook.com>
2023-10-26 09:40:54 +0400

    Creates empty processor that inherits from IProcessor and creates failing test for FizzProcessor to return fizz when a number is divisble by three

---
+++ HealthPartners.FizzBuzz/HealthPartners.FizzBuzz.Domain.UnitTests/Processors/FizzProcessorTests.cs
@@ -0,0 +1,23 @@
+﻿using HealthPartners.FizzBuzz.Domain.Processors;
+using NUnit.Framework;
+
+namespace HealthPartners.FizzBuzz.Domain.UnitTests.Processors
+{
+    internal class FizzProcessorTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ProcessMethod_WhenProvidedANumberDivisbleByThree_ReturnsFizz()
+        {
+            var fizzProcessor = new FizzProcessor();
+            var numberDivisbleByThree = 3;
+
+            var result = fizzProcessor.Process(numberDivisbleByThree);
+            Assert.AreEqual("Fizz", result);
+        }
+    }
+}
---
+++ HealthPartners.FizzBuzz/HealthPartners.FizzBuzz.Domain/Processors/FizzProcessor.cs
@@ -0,0 +1,14 @@
+﻿using HealthPartners.FizzBuzz.Domain.Interfaces;
+
+namespace HealthPartners.FizzBuzz.Domain.Processors
+{
+    public class FizzProcessor : IProcessor
+    {
+        public byte OrderConfig => throw new NotImplementedException();
+
+        public string? Process(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
