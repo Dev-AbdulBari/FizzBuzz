260ecca9f6dafee8147a1e691399bf717fbb6099
Abdul Bari <linked-blue@outlook.com>
2023-10-26 09:50:45 +0400

    Adds test for FizzProcess process method being passed a number not divisble by three returing null

--- HealthPartners.FizzBuzz/HealthPartners.FizzBuzz.Domain.UnitTests/Processors/FizzProcessorTests.cs
+++ HealthPartners.FizzBuzz/HealthPartners.FizzBuzz.Domain.UnitTests/Processors/FizzProcessorTests.cs
@@ -19,5 +19,15 @@ namespace HealthPartners.FizzBuzz.Domain.UnitTests.Processors
             var result = fizzProcessor.Process(numberDivisbleByThree);
             Assert.AreEqual("Fizz", result);
         }
+
+        [Test]
+        public void ProcessMethod_WhenProvidedANumberNotDivisbleByThree_ReturnsFizz()
+        {
+            var fizzProcessor = new FizzProcessor();
+            var numberDivisbleByThree = 4;
+
+            var result = fizzProcessor.Process(numberDivisbleByThree);
+            Assert.IsNull(result);
+        }
     }
 }
