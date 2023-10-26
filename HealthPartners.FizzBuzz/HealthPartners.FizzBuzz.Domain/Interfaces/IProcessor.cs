a3450316a6570f23e46890fdddbe465be63d6ed8
Abdul Bari <linked-blue@outlook.com>
2023-10-26 09:23:03 +0400

    Creates interface for a processor

---
+++ HealthPartners.FizzBuzz/HealthPartners.FizzBuzz.Domain/Interfaces/IProcessor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthPartners.FizzBuzz.Domain.Interfaces
+{
+    public interface IProcessor
+    {
+        byte OrderConfig { get; }
+
+        string? Process(int number);
+    }
+}
